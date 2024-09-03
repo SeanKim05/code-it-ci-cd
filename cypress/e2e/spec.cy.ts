4f53e9fdad4e09cbd5067d0347a7c6f8eaf82b1f
SeanKim05 <dbgus20609@gmail.com>
2024-09-03 18:49:34 +0900

    add: cypress case

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,17 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
+describe("Basic page checks", () => {
+  beforeEach(() => {
+    cy.visit("/"); // Cypress will prepend this with the baseUrl
+  });
 
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+  it('should have "Get started by" text on the home page', () => {
+    cy.contains("Get started by").should("exist");
+  });
+
+  it('should have "Docs" anchor with correct href', () => {
+    cy.get("a")
+      .contains("Docs")
+      .should("exist")
+      .and("have.attr", "href")
+      .and("eq", "/docs");
   });
 });
